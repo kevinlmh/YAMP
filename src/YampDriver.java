703b947e9df80aaea8dd667ff6669ee7bd941d99
Minghui Liu <minghui.liu@trincoll.edu>
2015-03-24 18:55:05 -0400

    Implement basic playback functions

---
+++ src/YampDriver.java
@@ -0,0 +1,185 @@
+import java.io.File;
+import java.util.Map;
+
+import javazoom.jlgui.basicplayer.BasicController;
+import javazoom.jlgui.basicplayer.BasicPlayer;
+import javazoom.jlgui.basicplayer.BasicPlayerEvent;
+import javazoom.jlgui.basicplayer.BasicPlayerException;
+import javazoom.jlgui.basicplayer.BasicPlayerListener;
+
+public class YampDriver implements BasicPlayerListener {
+	// BasicPlayer module
+	private BasicPlayer player = null;
+	// BasicController module
+	private BasicController control = null;
+	// UI module
+	private YampUI ui = null;
+	
+	// Path of current song that is opened
+	private String currentSongPath;
+	
+	/** 
+	 * Constructor. 
+	 */
+	public YampDriver() {
+		// Instantiate BasicPlayer.
+		player = new BasicPlayer();
+		// BasicPlayer is a BasicController.
+		control = (BasicController) player;	
+		// Register BasicPlayerTest to BasicPlayerListener events.
+		// It means that this object will be notified on BasicPlayer
+		// events such as : opened(...), progress(...), stateUpdated(...)
+		player.addBasicPlayerListener(this);
+		
+		ui = new YampUI("Yet Another Music Player", YampDriver.this);
+		ui.setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		YampDriver test = new YampDriver();
+		//test.play("/home/kevin/test.mp3");
+		//test.play(args[0]);
+	}
+	
+	public void play(String filename) {
+		try { 
+			// Open file, or URL or Stream (shoutcast, icecast) to play.
+			control.open(new File(filename));
+
+			control.play();
+
+			// Set Volume (0 to 1.0).
+			control.setGain(0.85);
+			// Set Pan (-1.0 to 1.0).
+			control.setPan(0.0);
+		} catch (BasicPlayerException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void stop() {
+		try {
+			control.stop();
+		} catch (BasicPlayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void pause() {
+		try {
+			control.pause();
+		} catch (BasicPlayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void resume() {
+		try {
+			control.resume();
+		} catch (BasicPlayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void skip(long bytes) {
+		try {
+			control.seek(bytes);
+		} catch (BasicPlayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void setCurrentSong(String filepath) {
+		currentSongPath = filepath;
+	}
+	
+	public void setVolume(int percentage) {
+		try {
+			control.setGain((double)percentage/100.0);
+		} catch (BasicPlayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/* Implementation of functions in BasicPlayerListener interface */
+	
+	
+	/**
+	 * Open callback, stream is ready to play.
+	 *
+	 * properties map includes audio format dependent features such as bit rate,
+	 * duration, frequency, channels, number of frames, VBR flag, ...
+	 *
+	 * @param stream
+	 *            could be File, URL or InputStream
+	 * @param properties
+	 *            audio stream properties.
+	 */
+	@Override
+	@SuppressWarnings("rawtypes")
+	public void opened(Object stream, Map properties) {
+		// Pay attention to properties. It's useful to get duration,
+		// bit rate, channels, even tag such as ID3v2.
+		System.out.println("opened : " + properties.toString());
+		int seconds = (int)((Long)(properties.get("duration"))/1000000);
+		ui.setTotalTime(seconds);
+	}
+
+	/**
+	 * * Progress callback while playing.
+	 *
+	 * This method is called severals time per seconds while playing. properties
+	 * map includes audio format features such as instant bit rate, microseconds
+	 * position, current frame number, ...
+	 *
+	 * @param bytesread
+	 *            from encoded stream.
+	 * @param microseconds
+	 *            elapsed (<b>reseted after a seek !</b>).
+	 * @param pcmdata
+	 *            PCM samples.
+	 * @param properties
+	 *            audio stream parameters.
+	 */
+	@Override
+	@SuppressWarnings("rawtypes")
+	public void progress(int bytesread, long microseconds, byte[] pcmdata, Map properties) {
+		// Pay attention to properties. It depends on underlying JavaSound SPI
+		// MP3SPI provides mp3.equalizer.
+//		System.out.println("progress : " + properties.toString());
+//		System.out.println("bytesread: " + bytesread + ", microseconds: " + microseconds);
+		int seconds = (int) (microseconds / 1000000);
+		ui.updateTime(seconds);
+	}
+
+	/**
+	 * Notification callback for basicplayer events such as opened, eom ...
+	 *
+	 * @param event
+	 */
+	@Override
+	public void stateUpdated(BasicPlayerEvent event) {
+		// Notification of BasicPlayer states (opened, playing, end of media,
+		// ...)
+		System.out.println("stateUpdated : " + event.toString());
+	}
+
+	/**
+	 * A handle to the BasicPlayer, plugins may control the player through the
+	 * controller (play, stop, ...)
+	 * 
+	 * @param controller
+	 *            : a handle to the player
+	 */
+	@Override
+	public void setController(BasicController controller) {
+		System.out.println("setController : " + controller);
+	}
+	
+}
---
+++ src/YampUI.java
@@ -0,0 +1,214 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+public class YampUI extends JFrame {
+	
+	// Swing compoenents declaration
+	private JButton btnOpen;
+	private JButton btnPlay;
+	private JButton btnPause;
+	private JButton btnResume;
+	private JButton btnStop;
+	private JButton btnSkip;
+	private JLabel lblFilePath;
+	private JLabel lblVolume;
+	private JLabel lblTime;
+	private JLabel lblTotal;
+	private JFileChooser fc;
+	private JTextField txtFilePath;
+	private JTextField txtSkip;
+	private JSlider sldVolume;
+	private JSlider sldTime;
+	// Yamp driver
+	private YampDriver driver;
+	// User selected file
+	private File selectedFile;
+	// Total time of current song
+	private int totalTime;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param title The title of the window
+	 * @param driver An instance of the YampDriver class
+	 */
+	public YampUI(String title, YampDriver driver) {
+		super(title);
+		initUI();
+		this.driver = driver;
+	}
+	
+	public void updateTime(int seconds) {
+		int min = seconds/60;
+		int sec = seconds%60;
+		lblTime.setText(Integer.toString(min) + ":" + Integer.toString(sec));
+		sldTime.setValue(100*seconds/totalTime);
+	}
+	
+	public void setTotalTime(int seconds) {
+		totalTime = seconds;
+		int min = seconds/60;
+		int sec = seconds%60;
+		lblTotal.setText(Integer.toString(min) + ":" + Integer.toString(sec));
+	}
+	
+
+	/**
+	 *  This method initializes the GUI elements
+	 */
+	public void initUI() {
+		//setTitle("Yamp");
+        setSize(460, 300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // optional: this line set the window to the center of screen
+        setLocationRelativeTo(null);
+        // No layout manager: absolute position
+        setLayout(null);
+        
+        // Initialize file chooser
+        fc = new JFileChooser();
+        
+        // Setup file path label
+        lblFilePath = new JLabel("File opened:");
+        lblFilePath.setBounds(50, 20, 200, 25);
+        add(lblFilePath);
+        
+        // Setup text field
+        txtFilePath = new JTextField(40);
+        txtFilePath.setBounds(50, 50, 360, 25);
+        add(txtFilePath);
+        
+        // Setup Open button
+        btnOpen = new JButton("Open");
+        btnOpen.setBounds(20, 100, 80, 25);
+        btnOpen.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            // when open button is clicked open a file chooser dialog
+        	int returnVal = fc.showOpenDialog(YampUI.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                selectedFile = fc.getSelectedFile();
+                //This is where a real application would open the file.
+                System.out.println("Opening: " + selectedFile.getPath() + ".");
+                txtFilePath.setText(selectedFile.getName());
+                driver.setCurrentSong(selectedFile.getPath());
+            } else {
+                System.out.println("Open command cancelled by user.");
+                txtFilePath.setText("No file selected");
+            }
+          }
+        });
+        add(btnOpen);
+
+        // Setup Play button
+        btnPlay = new JButton("Play");
+        btnPlay.setBounds(100, 100, 80, 25);
+        btnPlay.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	// when play button is clicked play the current open song
+            	driver.play(selectedFile.getPath());
+            }
+          });
+        add(btnPlay);
+
+        // Setup pause button
+        btnPause = new JButton("Pause");
+        btnPause.setBounds(180, 100, 80, 25);
+        btnPause.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	// when pause button is clicked pause the current open song
+            	driver.pause();
+            }
+          });
+        add(btnPause);
+       
+        // Setup resume button
+        btnResume = new JButton("Resume");
+        btnResume.setBounds(260, 100, 100, 25);
+        btnResume.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	// when stop button is clicked stop the current open song
+            	driver.resume();
+            }
+          });
+        add(btnResume);
+        
+        // Setup stop button
+        btnStop = new JButton("Stop");
+        btnStop.setBounds(360, 100, 80, 25);
+        btnStop.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	// when stop button is clicked stop the current open song
+            	driver.stop();
+            }
+          });
+        add(btnStop);
+        
+        // Setup volume label
+        lblVolume = new JLabel("Volume level: 85");
+        lblVolume.setBounds(50, 130, 140, 25);
+        add(lblVolume);
+        
+        // Setup volume slider
+        sldVolume = new JSlider();
+        sldVolume = new JSlider(0, 100, 85);
+        sldVolume.setBounds(180, 130, 200, 25);
+        sldVolume.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                lblVolume.setText("Volume level: " + sldVolume.getValue());
+                driver.setVolume(sldVolume.getValue());
+            }
+        });
+        add(sldVolume);
+
+        // Setup time label
+        lblTime = new JLabel("00:00");
+        lblTime.setBounds(50, 160, 40, 25);
+        add(lblTime);
+        
+        // Setup remaining time label
+        lblTotal = new JLabel("??:??");
+        lblTotal.setBounds(380, 160, 40, 25);
+        add(lblTotal);
+        
+        // Setup time slider
+        sldTime = new JSlider();
+        sldTime = new JSlider(0, 100, 0);
+        sldTime.setBounds(50, 180, 360, 25);
+        sldTime.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                
+            }
+        });
+        add(sldTime);
+
+        
+        // Setup text field
+        txtSkip = new JTextField(40);
+        txtSkip.setBounds(50, 210, 80, 25);
+        add(txtSkip);
+        
+        // Setup skip button
+        btnSkip = new JButton("Skip");
+        btnSkip.setBounds(140, 210, 80, 25);
+        btnSkip.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	driver.skip(Long.parseLong(txtSkip.getText()));
+            }
+          });
+        add(btnSkip);
+        
+        
+	}
+
+}
